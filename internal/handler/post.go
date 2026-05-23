9c59eb97b55d79cbc825a57d09e36b7d78757b36
Andrey Ponteleev <ionrussia@gmail.com>
2026-05-23 16:50:05 +0300

    models

--- internal/handler/post.go
+++ internal/handler/post.go
@@ -1,7 +1,15 @@
 package handler
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func Post(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("Это POST"))
+	body, error := io.ReadAll(req.Body)
+	if error != nil {
+		http.Error(res, error.Error(), http.StatusBadRequest)
+	}
+	res.Write([]byte(body))
 }

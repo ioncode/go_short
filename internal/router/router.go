af46582cd0feabeb2cbc852b99449254ff7e9853
Andrey Ponteleev <ionrussia@gmail.com>
2026-05-23 14:47:59 +0300

    content type middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -7,11 +7,23 @@ import (
 	"github.com/ioncode/go_short/internal/handler"
 )
 
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		contentType := r.Header.Get("Content-type")
+		if contentType != "text/plain" {
+			http.Error(w, "Content type not correct", http.StatusBadRequest)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func Serve() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc(`GET /`, handler.Get)
 	mux.HandleFunc(`POST /`, handler.Post)
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", middleware(mux)))
 }

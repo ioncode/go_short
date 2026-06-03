e5adea34285f5a3ac5f80e536a2f5a9116e84b40
Andrey Ponteleev <ionrussia@gmail.com>
2026-06-03 09:21:43 +0300

    detect content type only with length

--- internal/router/router.go
+++ internal/router/router.go
@@ -13,18 +13,25 @@ import (
 func middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		contentType := r.Header.Get("Content-type")
-		mediaType, _, err := mime.ParseMediaType(contentType)
-		if err != nil {
-			http.Error(w, "Invalid Content-Type header", http.StatusBadRequest)
-			log.Println("Error parsing content type:", contentType)
+		log.Println("Middleware processing request with length ", r.ContentLength)
+		if r.ContentLength > 700 {
+			http.Error(w, "Request entity too large", http.StatusRequestEntityTooLarge)
 			return
+		} else if r.ContentLength > 0 {
+			contentType := r.Header.Get("Content-type")
+			mediaType, _, err := mime.ParseMediaType(contentType)
+			if err != nil {
+				http.Error(w, "Invalid Content-Type header", http.StatusBadRequest)
+				log.Println("Error parsing content type:", contentType)
+				return
+			}
+			if mediaType != "text/plain" {
+				http.Error(w, "Content type not correct", http.StatusBadRequest)
+				log.Println("Invalid content type for request: ", contentType, mediaType)
+				return
+			}
 		}
-		if mediaType != "text/plain" {
-			http.Error(w, "Content type not correct", http.StatusBadRequest)
-			log.Println("Invalid content type for request: ", contentType, mediaType)
-			return
-		}
+
 		w.Header().Set("Content-Type", "text/plain")
 		next.ServeHTTP(w, r)
 	})
